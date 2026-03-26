b1fd091d36aa9fb45b1785881137e89a2e58d3c9
kanuysal <serkanuysal@msn.com>
2026-03-26 01:12:51 +0100

    Fix: Switch to static output for zero-config build & cleanup legacy links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://minalidya.es',
-  output: 'hybrid',
+  output: 'static',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en', 'fr', 'tr', 'it', 'de', 'nl', 'ru'],
